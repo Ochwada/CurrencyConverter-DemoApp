4b00c463e44609eb2a452cd407e9644a7381d8bc
ochwada <nakhulolinda@yahoo.com>
2025-07-15 15:35:34 +0200

    .env and project setup,

--- src/main/java/com/ochwada/currency_converter/CurrencyConverterApplication.java
+++ src/main/java/com/ochwada/currency_converter/CurrencyConverterApplication.java
@@ -1,13 +1,56 @@
 package com.ochwada.currency_converter;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Entry point for the Currency Converter application.
+ * This Spring Boot application loads environment variables from a `.env` file using the dotenv-java library.
+ * Specifically, it looks for the `MONGODB_URI` variable and sets it as a system property for use by Spring Data MongoDB.
+ *
+ * @author Ochwada
+ */
 @SpringBootApplication
 public class CurrencyConverterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CurrencyConverterApplication.class, args);
+    /**
+     * Main method that launches the Spring Boot application.
+     *
+     * @param args application arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyConverterApplication.class, args);
+    }
+
+    //=================================== .env SETTINGS ======================================================
+	static {
+		// Load environment variables from .env (ignore if .env is missing, e.g., on Heroku)
+		 Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+
+		// Retrieve and set MongoDB URI (Set the URI as a system property if found, so Spring Boot can use it)
+		String uri_mongo = dotenv.get("MONGODB_URI");
+		if (uri_mongo != null ){
+			System.setProperty("MONGODB_URI", uri_mongo);
+			System.out.println("✅ MONGODB_URI loaded and set.");
+		}else {
+			System.out.println("⚠\uFE0F MONGODB_URI not found in .env file. Skipping System.setProperty.");
+		}
+
+
+		// Retrieve and set Free Currency API key for Spring Boot
+		String free_currency = dotenv.get("FREECURRENCY_APIKEY");
+		if (free_currency != null){
+			System.setProperty("FREECURRENCY_APIKEY", free_currency);
+			System.out.println("✅ FREECURRENCY_APIKEY loaded and set." );
+		}else {
+			System.out.println("⚠\uFE0F FREECURRENCY_APIKEY not found in .env file. Skipping System.setProperty.");
+		}
+
 	}
+
+
 
 }
